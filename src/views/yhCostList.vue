7a7e47af9d2350843563f9dce1cf977fb6421fec
王健昀 <wangjy7639@163.com>
2020-02-05 18:26:59 +0800

    。

--- src/views/yhCostList.vue
+++ src/views/yhCostList.vue
@@ -7,24 +7,6 @@
           <el-button type="primary"  size="small">批量删除</el-button>
         </el-col>
       </el-row>
-      <el-row>
-        <!-- <el-col :span="24">
-          资产类别
-          <el-select v-model="zclb" placeholder="请选择">
-            <el-option
-              v-for="item in zclbs"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-          养护工程
-          起点桩号
-          终点桩号
-          自
-          至
-        </el-col> -->
-      </el-row>
     </div>
 </template>
 <script>
